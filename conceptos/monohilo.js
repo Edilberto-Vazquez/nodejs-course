dc1fde5ae5957f226269f9d88ad9496c4ebaf2ab
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-08-25 21:22:15 -0500

    monohilo

---
+++ conceptos/monohilo.js
@@ -0,0 +1,10 @@
+console.log("Hello, world");
+let i = 0;
+setInterval(() => {
+    console.log(i);
+    i++;
+    // if (i === 5) {
+    //   let a = 3 + z
+    // }
+}, 1000);
+console.log("segunda instruccion");
---
+++ conceptos/monohilo.ts
@@ -0,0 +1,13 @@
+console.log("Hello, world");
+
+let i: number = 0
+setInterval(() => {
+  console.log(i)
+  i++
+
+  // if (i === 5) {
+  //   let a = 3 + z
+  // }
+}, 1000);
+
+console.log("segunda instruccion")
